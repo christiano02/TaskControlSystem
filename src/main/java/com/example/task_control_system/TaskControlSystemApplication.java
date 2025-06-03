05bf3d2af340718027dba1a21ac6587e177766ff
Christiano <christianonascimento02@hotmail.com>
2025-06-02 22:23:07 -0300

    Created project

---
+++ src/main/java/com/example/task_control_system/TaskControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_control_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskControlSystemApplication.class, args);
+	}
+
+}

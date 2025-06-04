b9caa06eb48c5a9fe7d4ff1122d7ba3e1a6fd4d6
Christiano <christianonascimento02@hotmail.com>
2025-06-04 13:38:43 -0300

    feat: created method createUser

--- src/main/java/com/example/task_control_system/service/UserService.java
+++ src/main/java/com/example/task_control_system/service/UserService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Objects;
+
 @Service
 public class UserService {
 
@@ -18,5 +20,20 @@ public class UserService {
             User userExisting = userRepository.findById(id)
                     .orElseThrow(() -> new RuntimeException("User not found"));
             return new UserDTO(userExisting);
+    }
+    public User createUser(UserDTO dto){
+
+        User existing = userRepository.findByUserName(dto.getName());
+        if(Objects.nonNull(existing)){
+            throw new RuntimeException("Existing User");
+        }
+        User user = new User();
+        user.setId(dto.getId());
+        user.setName(dto .getName());
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
+        user.setProfile(dto.getProfile());
+
+        return new User(user.getId(), user.getName(), user.getEmail(),user.getPassword(), user.getProfile());
     }
 }

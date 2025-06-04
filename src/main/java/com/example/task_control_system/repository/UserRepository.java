f7e6fc111d119658aeed034eb8dd5b59b2308779
Christiano <christianonascimento02@hotmail.com>
2025-06-04 13:40:23 -0300

    fix: add abstract method findByUserName

--- src/main/java/com/example/task_control_system/repository/UserRepository.java
+++ src/main/java/com/example/task_control_system/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.example.task_control_system.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    User findByUserName(String login);
 }

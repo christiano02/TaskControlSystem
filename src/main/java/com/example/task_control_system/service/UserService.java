591bf50ecb3674154df329e428138a34cda19b00
Christiano <christianonascimento02@hotmail.com>
2025-06-06 20:23:18 -0300

    feat: config data base H2

--- src/main/java/com/example/task_control_system/service/UserService.java
+++ src/main/java/com/example/task_control_system/service/UserService.java
@@ -3,7 +3,6 @@ package com.example.task_control_system.service;
 import com.example.task_control_system.dto.UserDTO;
 import com.example.task_control_system.entity.User;
 import com.example.task_control_system.repository.UserRepository;
-import com.fasterxml.jackson.core.JsonEncoding;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -29,9 +28,13 @@ public class UserService {
 
         User userExisting = userRepository.findByUserName(dto.getUserName());
         if(Objects.nonNull(userExisting)){
+
             throw new RuntimeException("Existing User");
+
         }
+
         User user = new User();
+
         user.setId(dto.getId());
         user.setName(dto .getName());
         user.setEmail(dto.getEmail());
@@ -40,18 +43,26 @@ public class UserService {
         user.setProfile(dto.getProfile());
 
         return new User(user.getId(), user.getName(), user.getEmail(), user.getUserName(), user.getPassword(), user.getProfile());
+        
     }
+
     public User updateUser(Long id, UserDTO dto){
+
         User userUpdate = userRepository.findById(id).orElse(null);
+
         if(Objects.nonNull(userUpdate)){
+
             userUpdate.setId(dto.getId());
             userUpdate.setName(dto.getName());
             userUpdate.setEmail(dto.getEmail());
             userUpdate.setUserName(dto.getUserName());
             userUpdate.setPassword(passwordEncoder.encode(dto.getPassword()));
             userUpdate.setProfile(dto.getProfile());
+
             return userRepository.save(userUpdate);
+
         }
+
         return null;
     }
 }

4be05b4e728c1dbe078682ce6c82c0fa34b5cd12
Christiano <christianonascimento02@hotmail.com>
2025-06-07 20:45:47 -0300

    fix: Connection to PostgresSQL database configured with environment variables

--- src/main/java/com/example/task_control_system/config/SecurityConfig.java
+++ src/main/java/com/example/task_control_system/config/SecurityConfig.java
@@ -2,36 +2,13 @@ package com.example.task_control_system.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/h2-console/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .headers(header -> header
-                        .disable() // Isso desabilita todos os headers, incluindo frameOptions — útil só se for dev
-                )
-                .formLogin(form -> form
-                        .defaultSuccessUrl("/", true)
-                        .permitAll()
-                )
-                .logout(logout -> logout.permitAll());
-
-        return http.build();
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder(){

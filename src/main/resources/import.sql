934b225e423a979fe8ac5c114fdd9201909832b1
Christiano <christianonascimento02@hotmail.com>
2025-08-07 22:26:31 -0300

    fix: config error sintaxte in Sql Import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-insert into tb_user (id, name, email, password, profile) values (1, 'João Silva', 'joao@email.com', '123456', 'ADMIN');
-insert into tb_user (id, name, email, password, profile) values (2, 'Maria Souza', 'maria@email.com', 'abcdef', 'USER');
+insert into tb_user (id, name, email, password, role) values (1, 'João Silva', 'joao@email.com', '123456', 'ADMIN');
+insert into tb_user (id, name, email, password, role) values (2, 'Maria Souza', 'maria@email.com', 'abcdef', 'USER');
 
 insert into tb_task (id, title, description, creation_date, status, user_id) values (1, 'Estudar Java', 'Focar em Streams e Lambdas', CURRENT_TIMESTAMP, 'PENDING', 1);
 insert into tb_task (id, title, description, creation_date, status, user_id) values (2, 'Revisar Spring', 'Anotações @Service, @Repository...', CURRENT_TIMESTAMP, 'PENDING', 1);
